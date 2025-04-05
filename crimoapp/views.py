f9ff9f5630ae28b4ba3d1f4c907ec72e0e5ee2fa
julian hutchins <93027694+SpidersSuck@users.noreply.github.com>
2025-04-04 17:17:40 -0700

    Add files via upload

---
+++ crimoapp/views.py
@@ -0,0 +1,15 @@
+# views.py
+from django.shortcuts import render
+from .models import Disaster
+
+def home(request):
+    disasters = Disaster.objects.all()  # Fetch all disaster data
+    return render(request, 'crimohtml.html', {'disasters': disasters})
+
+def resources(request):
+    disasters = Disaster.objects.all()  # Fetch all disaster data
+    return render(request, 'resources.html', {'disasters': disasters})
+
+def dashboard(request):
+    disasters = Disaster.objects.all()
+    return render(request, 'dashboard.html', {'disasters': disasters})
