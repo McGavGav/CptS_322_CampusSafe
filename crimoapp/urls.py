36acd42d81acb6ad47a0337a34e8a41c92b19f24
Gilad28 <heitnergilad@gmail.com>
2025-04-06 14:44:22 -0700

    Added reports page, added notifications, synced the reports with the dashboard

--- crimoapp/urls.py
+++ crimoapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from crimoapp import views
+
 urlpatterns = [
     path('',views.home,name='home'),
     path('resources/',views.resources,name='resources'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path("report/", views.report, name="report"),
 ]
--- crimoapp/views.py
+++ crimoapp/views.py
@@ -1,6 +1,42 @@
 # views.py
 from django.shortcuts import render
 from .models import Disaster
+import json
+import os
+from .utils import send_notification
+
+
+def report(request):
+    if request.method == "POST":
+        report = {
+            "name": request.POST["name"],
+            "id": request.POST["id"],
+            "location": request.POST["location"],
+            "description": request.POST["description"],
+            "time": request.POST["time"]
+        }
+
+        file_path = "campus_safe_reports.json"
+
+        if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+            with open(file_path, "r") as file:
+                reports = json.load(file)
+        else:
+            reports = []
+        
+        reports.append(report)
+
+        with open(file_path, "w") as file:
+            json.dump(reports, file, indent=2)
+    
+        send_notification(
+            info=report['description'],
+            location=f"At: {report['location']}"
+        )
+
+    return render(request, "report.html")
+
+        
 
 def home(request):
     disasters = Disaster.objects.all()  # Fetch all disaster data
@@ -11,5 +47,12 @@ def resources(request):
     return render(request, 'resources.html', {'disasters': disasters})
 
 def dashboard(request):
-    disasters = Disaster.objects.all()
+    file_path = "campus_safe_reports.json"
+
+    if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+        with open(file_path, "r") as file:
+            disasters = json.load(file)
+    else:
+        disasters = []
+
     return render(request, 'dashboard.html', {'disasters': disasters})
