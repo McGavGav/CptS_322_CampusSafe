9c44234347d1887a1554fdfd752aedddcaa6f9f2
marissa <109318586+Marissacho@users.noreply.github.com>
2025-04-06 22:10:27 -0700

    Update urls.py

--- crimoapp/urls.py
+++ crimoapp/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import incidentMap
+from crimoapp import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('resources/', views.resources, name='resources'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('report/', views.report, name='report'),
+    path('registerUser/', views.register_user, name='register_user'),  # Fixed import
+    path('registration_success/', views.registration_success, name='registration_success'),
+    path('login/', views.login_user, name='login_user'),  # Add login view
     path('incident-map/', incidentMap, name='incidentMap'),
+
 ]
+
