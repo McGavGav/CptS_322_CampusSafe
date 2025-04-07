c5b5415c5d12857927571c7086a3854db9059bcd
marissa <109318586+Marissacho@users.noreply.github.com>
2025-04-06 22:08:10 -0700

    Update views.py

--- crimoapp/views.py
+++ crimoapp/views.py
@@ -7,5 +7,5 @@ def incidentMap (request):
   incident = Incident.objest.all()
   incident_json = json.dumps ([{'name : i.name, 'description' : i.description, 'latitude' : i.latitude, 'longitude' : i.longitude}
                                 for i in incident])
-  return render (request, 'IncidentMap.html', {'incident_json':incident_json})
+  return render (request, 'incidentMap.html', {'incident_json':incident_json})
   
