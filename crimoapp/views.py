65d0c2e10d89c4534612768f13c62af3c3e9df4f
marissa <109318586+Marissacho@users.noreply.github.com>
2025-04-06 21:59:13 -0700

    Create views.py

---
+++ crimoapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Incident
+import json
+
+
+def incidentMap (request):
+  incident = Incident.objest.all()
+  incident_json = json.dumps ([{'name : i.name, 'description' : i.description, 'latitude' : i.latitude, 'longitude' : i.longitude}
+                                for i in incident])
+  return render (request, 'IncidentMap.html', {'incident_json':incident_json})
+  
