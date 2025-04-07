a0c22a382e33d82baeedbb26edcc69d947b04c43
marissa <109318586+Marissacho@users.noreply.github.com>
2025-04-06 22:07:34 -0700

    Update urls.py

--- crimoapp/urls.py
+++ crimoapp/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from .views import IncidentMap
 
-url = [ path('incident-map/',IncidentMap, name = 'IncidentMap'),]
+urlpatterns = [
+    path('incident-map/', IncidentMap, name='IncidentMap'),
+]
